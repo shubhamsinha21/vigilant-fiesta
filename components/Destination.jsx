404fcc932b8783a3e1140f0b0faaeca74d6a48ec
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-01-19 09:17:12 +0530

    destination-images-constants

---
+++ components/Destination.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Destination() {
+    return (
+        <View>
+            <Text>Destination</Text>
+        </View>
+    )
+}
---
+++ constants/constants.js
@@ -0,0 +1,75 @@
+export const sortCategories = ["All", "Popular", "Recommended", "More"];
+
+export const categoriesData = [
+    {
+        title: "Ocean",
+        image: require("../assets/ocean.jpg")
+    },
+    {
+        title: "Mountain",
+        image: require("../assets/mountain.jpg")
+    },
+    {
+        title: "Beach",
+        image: require("../assets/beach.jpg")
+    },
+    {
+        title: "Hiking",
+        image: require("../assets/hiking.jpg")
+    },
+    {
+        title: "Forest",
+        image: require("../assets/forest.jpg")
+    },
+    {
+        title: "Camping",
+        image: require("../assets/camping.jpg")
+    },
+    {
+        title: "Sunset",
+        image: require("../assets/sunset.jpg")
+    }
+]
+
+export const destinations = [
+    {
+        title: "Maldives",
+        duration: "12 days",
+        distance: "10,000 KM",
+        price: 2100,
+        weather: "20 C",
+        shortDescription: "Topping the global list is the enchanting paradise of the Maldives in the Indian Ocean.",
+        longDescription: "Topping the global list is the enchanting paradise of the Maldives in the Indian Ocean. As Paton describes, With its overwater bungalows, crystal-clear waters and vibrant marine life, it’s no wonder that this tropical haven is at the pinnacle of travel dreams.",
+        image: require("../assets/destinations/maldives.jpg")
+    },
+    {
+        title: "Niagra Falss",
+        duration: "18 days",
+        distance: "12,000 KM",
+        price: 2800,
+        weather: "12 C",
+        shortDescription: "Coming in at a close second is the awe-inspiring Niagara Falls.",
+        longDescription: "Coming in at a close second is the awe-inspiring Niagara Falls. “The thundering waters at the border of Canada and the United States never fail to mesmerize visitors, providing an unforgettable bucket list experience",
+        image: require("../assets/destinations/niagra.jpg")
+    },
+    {
+        title: "Northern Lights",
+        duration: "20 days",
+        distance: "18,000 KM",
+        price: 3200,
+        weather: "10 C",
+        shortDescription: "Witnessing the ethereal Northern Lights dance across the Arctic skies.",
+        longDescription: "Witnessing the ethereal Northern Lights dance across the Arctic skies is a natural spectacle that ranks high on the bucket list for many adventure seekers.",
+        image: require("../assets/destinations/lights.jpg")
+    },
+    {
+        title: "Times Square",
+        duration: "10 days",
+        distance: "8,000 KM",
+        price: 1500,
+        weather: "16 C",
+        shortDescription: "Often referred to as “The Crossroads of the World,” Times Square is legendary for its annual New Year's Eve ball drop celebration.",
+        longDescription: "Often referred to as “The Crossroads of the World,” Times Square is legendary for its annual New Year's Eve ball drop celebration. “Times Square in the heart of New York City is synonymous with vibrant city life and dazzling billboards, making it a must-see for travelers.",
+        image: require("../assets/destinations/square.jpg")
+    }
+]
